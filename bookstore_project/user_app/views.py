43a60ce980d5bd47b9004579ec75bbaaac2b5886
rahulxqmoz <92361551+rahulxqmoz@users.noreply.github.com>
2024-05-21 01:50:59 +0530

    Update views.py forget password

--- bookstore_project/user_app/views.py
+++ bookstore_project/user_app/views.py
@@ -388,7 +388,7 @@ def user_logout(request):
 def send_forget_password_mail(email, token):
     try:
         subject = "Forgot password"
-        message = f'Hi, click the link to reset your password http://127.0.0.1:8000/reset-password/{token}/'
+        message = f'Hi, click the link to reset your password https://ebookstores.xyz/reset-password/{token}/'
         email_from = settings.EMAIL_HOST_USER
         recipient_list = [email, ]
         send_mail(subject, message, email_from, recipient_list)
